9b1ecf39f79db72a01822a14bb92c0b919945172
SZnine <1718882345@qq.com>
2025-12-18 19:14:19 +0800

    完成D8内容

---
+++ c++/string_test/string_test/string_test/string1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//£¨±àÒë»·¾³ £ºVS2015(v140) + X86 + Release£©
+int main()
+{
+	std::string s0;
+	std::cin >> s0;
+	printf("%s", s0.c_str());
+	return 0;
+}
